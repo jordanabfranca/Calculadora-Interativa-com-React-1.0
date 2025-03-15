0e43ab7c7aa9b789db125ee4197366507e5d0df8
jordanabfranca <jordanafranca@ufpi.edu.br>
2025-03-14 23:10:23 -0300

    feat: Implementando botão de calcular e adicionando um visor que mostra a operação

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -4,18 +4,30 @@ const Calculator = () => {
     const [input1, setInput1] = useState<string>("");
     const [input2, setInput2] = useState<string>("");
     const [result, setResult] = useState<number | string> ("");
+    const [expression, setExpression] = useState<string>("");
 
     //realizar operações
-    const operation = (operation: string) =>{
+    const operation = (op: string) =>{
+        if(!input1 || !input2){
+            setResult("Erro: Preencha os dois mumeros!");
+            return;
+        }
+        setExpression(`${input1} ${op} ${input2}`);
+    };
+
+    const calculaResult = () =>{
+        if(!expression) return;
+
+        const [n1, op, n2] = expression.split(" ");
         const num1 = parseFloat(input1);
         const num2 = parseFloat(input2);
 
         if (isNaN(num1) || isNaN(num2)){
             setResult("Erro: Números Inválidos")
-            return
+            return;
         }
 
-        switch (operation){
+        switch (op){
             case "+":
                 setResult (num1 + num2);
                 break;
@@ -25,13 +37,9 @@ const Calculator = () => {
             case "*":
                 setResult (num1 * num2);
                 break
-            case "/":
-                if (num2 === 0){
-                    setResult ("Erro: Divisao por zero")
-                }else{
-                    setResult(num1/num2);
-                }
-                break;
+                case "/":
+                    setResult(num2 !== 0 ? num1 / num2 : "Erro: Divisão por zero");
+                    break;
             case "^":
                 setResult(Math.pow(num1,num2));
                 break;
@@ -42,10 +50,8 @@ const Calculator = () => {
                     setResult(Math.sqrt(num1))
                 }
                 break;
-                default:
+            default:
                     setResult("Erro: Operação Inválida")
-            
-    
         }
     };
 
@@ -54,7 +60,14 @@ const Calculator = () => {
 return(
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
         <h1 className="text -2xl font-bold mmb-4"> Calculadora</h1>
+
+        {/*Visor Calculadora*/}
+        <div className="bg-white p-4 roundend-lg shadow-md w-64 text-rigt text-2xl font-mono mb-4 border border-gray-300 "> 
+            {expression || "  "}
+        </div>
+
         <div className="bg-white p-6 roundend-lg shadow-lg">
+            {/*Inputs */}
             <input
             type="number"
             value={input1}
@@ -71,6 +84,7 @@ return(
             placeholder="Número 2"
             />
 
+            {/*Botões operações*/}
             <div className=" grid grid--cols-3 gap-2">
                 <button
                 onClick={() => operation("+")}
@@ -97,8 +111,15 @@ return(
                 className="p-2 bg-blue-500 text-white roundend"> √</button>
             </div>
 
+            {/*Resultado*/}
+            <button 
+            onClick={calculaResult} className="w-full mt-4 p-2 bg-green-500 text-white roundend">
+            = 
+            </button>
+
+
             <div className="mt-4">
-                <p className="text-xl font-semibold">Resultado: {result}</p>
+                <p className="text-xl font-semibold"> {result !== "" ? `Resultado: ${result}`: ""}</p>
 
             </div>
 
