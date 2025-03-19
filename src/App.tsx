cfca74689663aa6a7f58897301bab5737117d27b
jordanabfranca <jordanafranca@ufpi.edu.br>
2025-03-19 20:28:19 -0300

    Feat: Incluindo a página Home e React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Calculator from "./components/Calculator";
+import Home from "./components/home";
 
 function App() {
-  return <Calculator />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
